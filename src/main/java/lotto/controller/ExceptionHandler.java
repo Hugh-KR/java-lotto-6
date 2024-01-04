37bf27f59093eda288740c779c99554d8c495e0a
Hugh <lee2317884@gmail.com>
2024-01-04 09:49:24 +0900

    test: 소나 클라우드 작동 확인을 위한 ExceptionHandler 코드 변경

--- src/main/java/lotto/controller/ExceptionHandler.java
+++ src/main/java/lotto/controller/ExceptionHandler.java
@@ -2,14 +2,11 @@ package lotto.controller;
 
 import java.util.function.Supplier;
 import lotto.exception.custom.CustomIllegalArgumentException;
-import lotto.exception.custom.CustomIllegalStateAmountException;
-import lotto.exception.custom.CustomNullPointAmountException;
-import lotto.exception.custom.CustomNumberFormatAmountException;
 
 public class ExceptionHandler {
 
     private static final ExceptionHandler EXCEPTION_HANDLER = new ExceptionHandler();
-    private static final String ERROR_PREFIX = "[ERROR] ";
+    private static final String ERROR_PREFIX = "[ERROR] 에러 출력";
 
     private ExceptionHandler() {
     }
@@ -22,8 +19,7 @@ public class ExceptionHandler {
         while (true) {
             try {
                 return supplier.get();
-            } catch (CustomNumberFormatAmountException | CustomNullPointAmountException |
-                     CustomIllegalArgumentException | CustomIllegalStateAmountException e) {
+            } catch (CustomIllegalArgumentException e) {
                 System.out.println(ERROR_PREFIX + e.getMessage());
             }
         }
